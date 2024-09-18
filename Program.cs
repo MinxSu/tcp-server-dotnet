05be5308ebe3eee5c5a79475e4e66bca9d4756d7
MinXin Su <minxinsu@MinXindeMacBook-Pro.local>
2024-09-18 18:09:27 +0800

    tcp service

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class TcpService
+{
+    static async Task Main(string[] args)
+    {
+        int port = 8888;
+        TcpListener listener = new TcpListener(IPAddress.Any, port);
+
+        try
+        {
+            listener.Start();
+            Console.WriteLine($"TCP Server Start:{port}");
+
+            while (true)
+            {
+                TcpClient client = await listener.AcceptTcpClientAsync();
+                _ = HandleClientAsync(client);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Exception: {ex.Message}");
+        }
+        finally
+        {
+            listener.Stop();
+        }
+    }
+
+    static async Task HandleClientAsync(TcpClient client)
+    {
+        Console.WriteLine($"client connected: {client.Client.RemoteEndPoint}");
+
+        using (NetworkStream stream = client.GetStream())
+        {
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+
+            while ((bytesRead = await stream.ReadAsync(buffer)) != 0)
+            {
+                string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                Console.WriteLine($"Received Message: {message}");
+
+                byte[] response = Encoding.UTF8.GetBytes($"Server Received Message: {message}");
+                await stream.WriteAsync(response);
+            }
+        }
+
+        Console.WriteLine($"disconnected: {client.Client.RemoteEndPoint}");
+        client.Close();
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# .NET 6 TCP Server Sample 
+
+### Test
+start up service
+```bash
+dotnet run
+```
+test as client
+```bash
+# windows 
+telnet localhost 8888
+# mac
+nc localhost 8888
+```
